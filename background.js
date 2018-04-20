6904f664c4d1b1bc0724539e79221ea008be8104
Shane Haggerty <shanehaggerty@umail.ucsb.edu>
2018-04-20 01:44:51 -0700

    removed unneccessary callback and color property

--- background.js
+++ background.js
@@ -5,9 +5,7 @@
 'use strict';
 
 chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757', volume: 1.0}, function() {
-    console.log('The color is green.');
-  });
+  chrome.storage.sync.set({volume: 1.0});
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
