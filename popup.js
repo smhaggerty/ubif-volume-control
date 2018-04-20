0579b3a33a60658656f1aa1e783100e9ce071a35
Shane Haggerty <shanehaggerty@umail.ucsb.edu>
2018-04-20 01:47:56 -0700

    initialize slider value to global volume, added slider event handler to update global volume when slider is moved

--- popup.js
+++ popup.js
@@ -14,14 +14,15 @@ chrome.storage.sync.get('color', data => {
 });
 
 chrome.storage.sync.get('volume', data => {
-  volumeIndicator.setAttribute("value", data.volume)
-
+  volumeIndicator.setAttribute("value", data.volume);
+  changeVolume.value = data.volume
 });
 
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-    chrome.tabs.executeScript(
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
+changeVolume.oninput = function(event)  {
+  const volume = event.srcElement.value / 10
+  volumeIndicator.value =  volume
+  chrome.storage.sync.set({volume: volume})
+  chrome.storage.sync.get(data => console.log(data))
 };
 
 chrome.storage.sync.get('color', data => {
