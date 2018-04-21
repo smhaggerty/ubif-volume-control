a4a7635a2639deeb5d3570f215d2f57ce04e8608
Shane Haggerty <shanehaggerty@umail.ucsb.edu>
2018-04-21 01:57:45 -0700

    update numeric volume display on slider input

--- popup.js
+++ popup.js
@@ -6,13 +6,16 @@
 
 let changeColor = document.getElementById('changeColor');
 let changeVolume = document.getElementById('changeVolume');
+let volumeDisplay = document.getElementById('volumeDisplay');
 
 chrome.storage.sync.get(data => {
   changeVolume.value = data.volume * 100;
+  volumeDisplay.innerHTML = data.volume * 100;
 });
 
 changeVolume.oninput = function(event)  {
   let volume = event.srcElement.value / 100;
+  volumeDisplay.innerHTML = Math.floor(volume * 100);
   chrome.storage.sync.set({volume: volume});
   chrome.storage.sync.get(data => console.log(data));
 };
