383dd48ccaf726bd8e7145a84b4fca6d49b5370f
Shane Haggerty <shanehaggerty@umail.ucsb.edu>
2018-04-27 01:21:12 -0700

    refactored; update volume and run audio script on all open tabs on volume changeVolume event

--- popup.js
+++ popup.js
@@ -10,12 +10,25 @@ let volumeDisplay = document.getElementById('volumeDisplay');
 
 chrome.storage.sync.get(data => {
   changeVolume.value = data.volume * 100;
-  volumeDisplay.innerHTML = data.volume * 100;
+  volumeDisplay.innerHTML = Math.floor(data.volume * 100);
 });
 
 changeVolume.oninput = function(event)  {
+  updateVolume(event)
+  adjustOpenTabVolume()
+};
+
+const updateVolume = function(event) {
   let volume = event.srcElement.value / 100;
   volumeDisplay.innerHTML = Math.floor(volume * 100);
   chrome.storage.sync.set({volume: volume});
+};
+
+const adjustOpenTabVolume = function() {
   chrome.storage.sync.get(data => console.log(data));
+  chrome.tabs.query({}, function (tabs) {
+    for (var i = 0; i < tabs.length; i++) {
+        chrome.tabs.executeScript(tabs[i].id, {file: "audio.js"});
+    }
+  });
 };
